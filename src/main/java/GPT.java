5f37522c8ea6af571af67b79345d101a78a3db83
darren <darrentoh50@gmail.com>
2026-08-21 15:39:12 +0800

    first commit

---
+++ src/main/java/GPT.java
@@ -0,0 +1,18 @@
+public class GPT {
+    public static void main(String[] args) {
+        String banner = "  ____ ____ _____ \n"
+                + " / ___|  _ \\_   _|\n"
+                + "| |  _| |_) || |  \n"
+                + "| |_| |  __/ | |  \n"
+                + " \\____|_|    |_|  \n";
+        String line = "____________________________________________________________";
+
+        System.out.println(line);
+        System.out.println(banner);
+        System.out.println("Hello! I'm GPT.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
