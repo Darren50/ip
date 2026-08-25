9058b0408ea7dbc557758bc5421cbab9b1b0cca7
darren <darrentoh50@gmail.com>
2026-08-25 11:59:04 +0800

    feat: added echo

--- src/main/java/GPT.java
+++ src/main/java/GPT.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class GPT {
     public static void main(String[] args) {
         String banner = "  ____ ____ _____ \n"
@@ -7,12 +9,30 @@ public class GPT {
                 + " \\____|_|    |_|  \n";
         String line = "____________________________________________________________";
 
+
+        Scanner in = new Scanner(System.in); //get input
+
         System.out.println(line);
         System.out.println(banner);
         System.out.println("Beep Boop, I'm GPT.");
-        System.out.println("I'm gonna use all your tokens today.");
+        System.out.println("What's cooking, I'm gonna use all your tokens today.");
+        System.out.println(line);
+        System.out.println();
+        while(true){
+            String input = in.nextLine();
+            if (input.equals("bye")){
+                break;
+            } else{
+                System.out.println(line);
+                System.out.println(input);
+                System.out.println(line);
+            }
+        }
+
         System.out.println(line);
         System.out.println("Bye. Make sure to buy more tokens");
         System.out.println(line);
+
+
     }
 }
