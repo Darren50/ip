b33105db1c97c31c801244268425a26d5438f666
darren <darrentoh50@gmail.com>
2026-08-25 13:42:42 +0800

    feat: mark/unmark using class

--- src/main/java/GPT.java
+++ src/main/java/GPT.java
@@ -8,12 +8,11 @@ public class GPT {
                 + "| |_| |  __/ | |  \n"
                 + " \\____|_|    |_|  \n";
         String line = "____________________________________________________________";
-        String[] descriptions = new String[100];
-        boolean[] isDone = new boolean[100];
+        Task[] tasks = new Task[100];
 
         int count = 0;
 
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in); // get input
 
         System.out.println(line);
         System.out.println(banner);
@@ -31,27 +30,26 @@ public class GPT {
             } else if (firstCommand.equals("list")) {
                 System.out.println("Here are the tasks in your list: ");
                 for (int i = 0; i < count; i++) {
-                    String icon = isDone[i] ? "[X]" : "[ ]";
-                    System.out.println(" " + (i + 1) + "." + icon + " " + descriptions[i]);
+                    System.out.println(" " + (i + 1) + "." + tasks[i].getIcon() + " " + tasks[i].getDescription());
                 }
             } else if (firstCommand.equals("mark")) {
                 int taskNo = Integer.parseInt(words[1]) - 1;
-                isDone[taskNo] = true;
+                tasks[taskNo].setAsDone();
                 System.out.println("Beep boop, task has been marked.");
-                System.out.println("[X] " + descriptions[taskNo]);
+                System.out.println(tasks[taskNo].getIcon() + " " + tasks[taskNo].getDescription());
             } else if (firstCommand.equals("unmark")) {
                 int taskNo = Integer.parseInt(words[1]) - 1;
-                isDone[taskNo] = false;
+                tasks[taskNo].setAsNotDone();
                 System.out.println("Beep boop, task has been unmarked.");
-                System.out.println("[ ] " + descriptions[taskNo]);
+                System.out.println(tasks[taskNo].getIcon() + " " + tasks[taskNo].getDescription());
             } else {
-                descriptions[count] = input;
-                isDone[count] = false;
+                tasks[count] = new Task(input);
                 count++;
                 System.out.println("added: " + input);
             }
             System.out.println(line);
         }
+
 
         System.out.println("Bye. Make sure to buy more tokens");
         System.out.println(line);
